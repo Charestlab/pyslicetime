b9b515ca81985af43d158aa64486ad9cecd60621
Jasper <vandejjf@bham.ac.uk>
2019-07-19 18:56:26 +0100

    bumped version to 0.2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('requirements.txt') as reqfile:
 
 setup(
     name='pyslicetime',
-    version='0.1',
+    version='0.2',
     description='Python Slice Timing Correction',
     url='https://github.com/Charestlab/pyslicetime',
     long_description='',
